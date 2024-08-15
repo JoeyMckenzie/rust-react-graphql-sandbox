7e95e9f83febb90d6daf1f2db60db12763ae3faa
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-08-15 11:39:10 -0700

    feat: add shad

--- hop-query-ui/src/App.tsx
+++ hop-query-ui/src/App.tsx
@@ -1,56 +1,56 @@
-import { useQuery, gql } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import "./App.css";
 
 type Brewery = {
-	id: number;
-	name: string;
-	description: string;
-	location: string;
-	year_establised: number;
+    id: number;
+    name: string;
+    description: string;
+    location: string;
+    year_establised: number;
 };
 
 const GET_BREWERIES = gql`
-  query GetBreweries {
-    breweries {
-      id
-      name
-      description,
-	  location,
-	  yearEstablished
+    query GetBreweries {
+        breweries {
+        id
+        name
+        description,
+        location,
+        yearEstablished
+        }
     }
-  }
 `;
 
 function DisplayBreweries() {
-	const { loading, error, data } = useQuery<{ breweries: Brewery[] }>(
-		GET_BREWERIES,
-	);
+    const { loading, error, data } = useQuery<{ breweries: Brewery[] }>(
+        GET_BREWERIES,
+    );
 
-	if (loading) {
-		return <p>Loading...</p>;
-	}
+    if (loading) {
+        return <p>Loading...</p>;
+    }
 
-	if (error) {
-		return <p>Error : {error.message}</p>;
-	}
+    if (error) {
+        return <p>Error : {error.message}</p>;
+    }
 
-	return data?.breweries.map(({ id, name, description }) => (
-		<div key={id}>
-			<h3>{name}</h3>
-			<br />
-			<b>About this location:</b>
-			<p>{description}</p>
-			<br />
-		</div>
-	));
+    return data?.breweries.map(({ id, name, description }) => (
+        <div key={id}>
+            <h3>{name}</h3>
+            <br />
+            <b>About this location:</b>
+            <p>{description}</p>
+            <br />
+        </div>
+    ));
 }
 
 export default function App() {
-	return (
-		<>
-			<h2>My first Apollo app 🚀</h2>
-			<br />
-			<DisplayBreweries />
-		</>
-	);
+    return (
+        <>
+            <h2>My first Apollo app 🚀</h2>
+            <br />
+            <DisplayBreweries />
+        </>
+    );
 }
--- hop-query-ui/src/main.tsx
+++ hop-query-ui/src/main.tsx
@@ -7,16 +7,16 @@ import "./index.css";
 const root = document.getElementById("root");
 
 const client = new ApolloClient({
-	uri: import.meta.env.VITE_API_BASE_URL,
-	cache: new InMemoryCache(),
+    uri: import.meta.env.VITE_API_BASE_URL,
+    cache: new InMemoryCache(),
 });
 
 if (root) {
-	createRoot(root).render(
-		<StrictMode>
-			<ApolloProvider client={client}>
-				<App />
-			</ApolloProvider>
-		</StrictMode>,
-	);
+    createRoot(root).render(
+        <StrictMode>
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
+        </StrictMode>,
+    );
 }
